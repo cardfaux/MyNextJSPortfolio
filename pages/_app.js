e05b65dc758c6e2f5a5e676df76c809df7e056a9
cardfaux <cardfaux@tutanota.com>
2020-11-27 21:16:44 -0600

    added express to run the server

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import App from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.scss';
 
@@ -8,15 +11,24 @@ import Footer from '../components/Shared/Footer';
 const MyApp = ({ Component, pageProps }) => {
   console.log(Component);
   return (
-    <div className="portfolio-app">
+    <div className='portfolio-app'>
       <Navbar />
-      { Component.name === 'Home' && <Hero /> }
-      <div className="container">
+      {Component.name === 'Home' && <Hero />}
+      <div className='container'>
         <Component {...pageProps} />
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
+
+MyApp.getInitialProps = async (context) => {
+  const initialProps = App.getInitialProps && (await App.getInitialProps(context));
+  console.log(initialProps);
+
+  return {
+    pageProps: { appData: 'Hello _App Component', ...initialProps.pageProps },
+  };
+};
 
 export default MyApp;
