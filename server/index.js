0a5eef8acee6bf874080371e669a5f8fe07db8b6
cardfaux <cardfaux@tutanota.com>
2020-11-28 08:56:20 -0600

    added launch.json file for debugging and chalk for terminal colors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const next = require('next');
+const chalk = require('chalk');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -16,6 +17,6 @@ app.prepare().then(() => {
   server.listen(port, (error) => {
     if (error) throw error;
 
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log(chalk.bgCyanBright.magentaBright(`> Ready on http://localhost:${port}`));
   });
 });
