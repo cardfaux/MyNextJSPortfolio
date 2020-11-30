63f1af81fe756d9277776d6ddead1de44534ce5a
cardfaux <cardfaux@tutanota.com>
2020-11-30 07:22:50 -0600

    added apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.scss';
 
@@ -7,17 +8,24 @@ import Navbar from '../components/Shared/Navbar';
 import Hero from '../components/Shared/Hero';
 import Footer from '../components/Shared/Footer';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+});
+
 const MyApp = ({ Component, pageProps }) => {
   console.log(Component);
   return (
-    <div className='portfolio-app'>
-      <Navbar />
-      {Component.name === 'Home' && <Hero />}
-      <div className='container'>
-        <Component {...pageProps} />
+    <ApolloProvider client={client}>
+      <div className='portfolio-app'>
+        <Navbar />
+        {Component.name === 'Home' && <Hero />}
+        <div className='container'>
+          <Component {...pageProps} />
+        </div>
+        {Component.name === 'Home' && <Footer />}
       </div>
-      {Component.name === 'Home' && <Footer />}
-    </div>
+    </ApolloProvider>
   );
 };
 
